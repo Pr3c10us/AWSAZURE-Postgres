99aeca471124ed8b5648593d3e0f8228a43634a0
Pr3c10us <rahul@constient.com>
2022-11-19 18:51:20 +0100

    az client sec

--- providers.tf
+++ providers.tf
@@ -28,6 +28,6 @@ provider "azurerm" {
   features {}
   subscription_id = var.az_subscription_id
   client_id       = var.az_client_id
-  client_secret   = var.az_client_secret
+  # client_secret   = var.az_client_secret
   tenant_id       = var.az_tenant_id
 }
