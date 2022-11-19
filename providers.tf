ffb64f9c193b6a0125987c9586aad31386c9d358
Pr3c10us <rahul@constient.com>
2022-11-19 19:04:00 +0100

    a

--- providers.tf
+++ providers.tf
@@ -26,8 +26,8 @@ provider "aws" {
 provider "azurerm" {
   # Configuration options
   features {}
-  subscription_id = var.az_subscription_id
-  client_id       = var.az_client_id
-  # client_secret   = var.az_client_secret
-  tenant_id       = var.az_tenant_id
+  # subscription_id = var.az_subscription_id
+  # client_id       = var.az_client_id
+  # # client_secret   = var.az_client_secret
+  # tenant_id       = var.az_tenant_id
 }
