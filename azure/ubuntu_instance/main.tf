e0e8d528af6d4432fc879fcb0c7c7f48c0d2b448
Pr3c10us <rahul@constient.com>
2022-11-19 17:38:07 +0100

    changed resource grp ocation

--- azure/ubuntu_instance/main.tf
+++ azure/ubuntu_instance/main.tf
@@ -1,3 +1,7 @@
+resource "azurerm_resource_group" "rg" {
+  name      = var.resource_group_name
+  location  = var.resource_group_location
+}
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "myterraformnsg" {
