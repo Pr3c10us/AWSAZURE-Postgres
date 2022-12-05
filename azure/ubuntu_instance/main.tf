d0795565aef904d0a183e22f2bebb095f02cc91a
Pr3c10us <rahul@constient.com>
2022-12-05 17:00:06 +0100

    save

--- azure/ubuntu_instance/main.tf
+++ azure/ubuntu_instance/main.tf
@@ -1,8 +1,32 @@
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "myterraformnsg" {
-  name                = "NetworkSecurityGroup"
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "myPublicIP"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = var.resource_group_location
+  resource_group_name =  var.resource_group_name
 
   security_rule {
     name                       = "SSH"
@@ -11,71 +35,48 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    # Set destination port range to 22 for SSH and 5432 for PostgreSQL
-    destination_port_ranges     = ["22","5432"]
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
 
-resource "azurerm_public_ip" "myterraformpublicip" {
-  name                = "PublicIP"
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "myNIC"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-}
-
-# Create network interface
-resource "azurerm_network_interface" "myterraformnic" {
-  name                = "AZ_NIC"
-  location            = var.resource_group_location
-  resource_group_name =  var.resource_group_name
 
   ip_configuration {
-    name                          = "myNicConfiguration"
-    subnet_id                     = var.public_subnet_id
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
   }
 }
 
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.myterraformnic.id
-  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
-}
-
-# Generate random text for a unique storage account name
-resource "random_id" "randomId" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group =  var.resource_group_name
-  }
-
-  byte_length = 8
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
 # Create storage account for boot diagnostics
-resource "azurerm_storage_account" "mystorageaccount" {
-  name                     = "diag${random_id.randomId.hex}"
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
   location                 = var.resource_group_location
-  resource_group_name      =  var.resource_group_name
+  resource_group_name      = var.resource_group_name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# # Create (and display) an SSH key
-# resource "tls_private_key" "example_ssh" {
-#   algorithm = "RSA"
-#   rsa_bits  = 4096
-# }
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
   name                  = "azure-pg"
   location              = var.resource_group_location
-  resource_group_name   =  var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
   size                  = "Standard_D2ds_v4"
   admin_username        = "ubuntu"
 
