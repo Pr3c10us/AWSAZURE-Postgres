bd8919d7e5d9325edd1296e9b242cb793fcbbb50
Pr3c10us <rahul@constient.com>
2022-12-07 10:06:41 +0100

    d

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ module "aws_instance" {
   aws_vpc_id        = module.aws_vpc.vpc_id
 
   # Application Definition 
-  app_name        = "aws-postgres-db-primary" # Do NOT enter any spaces
+  app_name        = "aws-postgres-db-p" # Do NOT enter any spaces
   app_environment = "dev"       # Dev, Test, Staging, Prod, etc
 
   # Linux Virtual Machine
